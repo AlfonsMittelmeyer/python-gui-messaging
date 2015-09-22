ccd0800882e0adbec6a3eb0c9614b7ed4e69d003
AlfonsMittelmeyer <a.mittelmeyer@googlemail.com>
2015-09-22 15:35:32 +0200

    Add GuiDesigner/

---
+++ GuiDesigner/guidesigner/CreateAndLayout.py
@@ -0,0 +1,7 @@
+def main(parent):
+
+	LabelFrame('Options',text="""Options""",link="guidesigner/Options.py")
+	pack(fill=X,anchor=N)
+
+	Frame('LayoutShort',link="guidesigner/LayoutShort.py")
+	pack(side='bottom',anchor='n',fill='x')
